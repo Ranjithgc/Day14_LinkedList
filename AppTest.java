f058ab0a7180adcab34c3db2a5c8e159b903f3f1
Ranjithgc <ranjith.amca.16@acharya.ac.in>
2021-06-07 17:08:24 +0530

    Adding TestCases

--- AppTest.java
+++ AppTest.java
@@ -56,5 +56,18 @@ public class AppTest {
         		 		  myList.getTail().equals(myFirstNode);
          	Assert.assertEquals(10, 10);
     	}    
+
+	/**
+	* ability to insert element after given value node key
+	*/
+	@Test
+	public void insertNumberAfterGivenValue() {
+		LinkedList<Integer> linkedList = new LinkedList<Integer>();
+		Integer a = 56, b = 30, c = 70 ;
+		linkedList.append(a);
+		linkedList.append(c);
+		linkedList.insertNumberAfterGivenNodeKey(b, a);
+		Assert.assertEquals(true, linkedList.findNode(b));
+	}
 	
 }
